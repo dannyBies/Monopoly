89756cf5e20e0b16639572b6f2de492700d3f08e
Dushyant Priyadarshee <dushyant.priyadarshee@sharp-ls.eu>
2018-12-26 23:39:13 +0000

    Format document.

--- Monopoly/Model/Dice.cs
+++ Monopoly/Model/Dice.cs
@@ -1,29 +1,34 @@
 ﻿using System;
 
-namespace Monopoly.Model {
-	/// <summary>
-	/// class that throws two dices in the game
-	/// </summary>
-	public class Dice {
-		public int FirstDice { get; set; }
-		public int SecondDice { get; set; }
-		public bool HasBeenThrown { get; set; }
-		public Random Random { get; set; }
+namespace Monopoly.Model
+{
+    /// <summary>
+    /// class that throws two dices in the game
+    /// </summary>
+    public class Dice
+    {
+        public int FirstDice { get; set; }
+        public int SecondDice { get; set; }
+        public bool HasBeenThrown { get; set; }
+        public Random Random { get; set; }
 
 
-		public Dice() {
-			HasBeenThrown = false;
-			Random = new Random();
-		}
+        public Dice()
+        {
+            HasBeenThrown = false;
+            Random = new Random();
+        }
 
-		public int ThrowDice() {
-			FirstDice = Random.Next(1 , 6);
-			SecondDice = Random.Next(1 , 6);
-			return (FirstDice + SecondDice);
-		}
+        public int ThrowDice()
+        {
+            FirstDice = Random.Next(1, 6);
+            SecondDice = Random.Next(1, 6);
+            return (FirstDice + SecondDice);
+        }
 
-		public bool IsDouble() {
-			return (FirstDice == SecondDice);
-		}
-	}
+        public bool IsDouble()
+        {
+            return (FirstDice == SecondDice);
+        }
+    }
 }
